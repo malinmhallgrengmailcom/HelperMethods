d2ca5120b4b85f294b615c6e14bb963d98504fb2
Malin Hallgren <malin.m.hallgren@gmail.com>
2025-09-19 14:12:43 +0200

    basic methods and seetup

---
+++ UserInputMethods.cs
@@ -0,0 +1,40 @@
+﻿namespace UsefulMethods
+{
+    public static class HelperMethodsInput
+    {
+        public static int UserInputInt()
+        {
+            bool validInput = false;
+            int result = 0;
+
+            while (!validInput) 
+            {
+                Console.WriteLine("Please input a number:");
+                validInput = int.TryParse(Console.ReadLine(), out result);
+            }
+
+            return result;
+        }
+
+        public static int UserInputIntRange(int minRange, int maxRange)
+        {
+            //TODO
+        }
+
+        public static string UserInputStringRange(int minRange, int maxRange)
+        {
+            //TODO
+        }
+    }
+
+    public static class HelpperMethodsFormat
+    {
+        public static string[] SplitBySpace(string inputString)
+        {
+            inputString = inputString.Trim();
+            string[] splitString = inputString.Split(' ');
+
+            return splitString;
+        }
+    }
+}
